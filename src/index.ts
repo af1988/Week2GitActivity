6d35075f5a52af29c5bb963408a940985b2ffbec
af1988 <aikorf2002@gmail.com>
2024-09-13 11:29:19 -0400

    implement multiply operation

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,5 @@ console.log(`result_add = ${result_add}`);
 const result_subtract = calc.subtract(5, 4);
 console.log(`result_subtract = ${result_subtract}`);
 
+const result_multi = calc.multiply(5, 4);
+console.log(`result_multi = ${result_multi}`);
